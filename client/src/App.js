008d82a62708976ec101ba8be00e8dd1e492b068
gabeaster <gab.easter@live.com>
2020-04-23 16:00:58 -0400

    silly, already built update movie. had a problem, fixed the problem. mvp met

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import axios from "axios";
+import UpdateMovie from "./Movies/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -41,7 +42,7 @@ const App = () => {
       </Route>
 
       <Route path="/update-move/:id">
-        <UpdateMovei movieList={movieList} setMovieList={setMovieList} />
+        <UpdateMovie movieList={movieList} setMovieList={setMovieList} />
       </Route>
     </>
   );
